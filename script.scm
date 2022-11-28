8f5dada958193e768d19e2accae2591dea903ac4
Pranav Anurag <anuragpranav@gmail.com>
2022-11-28 18:21:53 +0530

    script.scm - 1.32

--- script.scm
+++ script.scm
@@ -1,27 +1,12 @@
-(define (sq x) (* x x))
+(define (accumulate combiner null-value term a next b)
+  (if (> a b) null-value
+    (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+  
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
 
 (define (product term a next b)
-  (if (> a b) 1
-    (* (term a) (product term (next a) next b))))
+  (accumulate * 1 term a next b))
 
-(define (factorial n)
-  (product
-    (lambda (x) x) 1 1+ n))
-
-(define (product-iterative term a next b)
-  (define product-iter current result) (* (term current) (product-iter (next current) result))
-  (product-iter a 1))
-
-;; wallis product is rewritten as (/ pi 2) = (* (/ 2 1) (/ 2 3) (/ 4 3) (/ 4 5) (/ 6 5) (/ 6 7)..)
-;; (* (/ 4 3) (/ 16 15) (/ 36 35))
-;; this is used to create the parameter 'term'
-(define (pi-product n)
-  (product (lambda (x) (/
-        (* 4.0 (sq x))
-        (- (* 4 (sq x)) 1)))
-    1
-    1+
-    n))
-
-
-(* 2.0 (pi-product 1000))
+(sum (lambda (x) x) 1 1+ 100)
+(sum (lambda (x) (* x x)) 1 1+ 10)
