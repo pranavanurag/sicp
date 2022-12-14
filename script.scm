349e4e266793791f63c54e1ed11462faec5c0b61
Pranav Anurag <anuragpranav@gmail.com>
2022-12-14 15:14:33 +0530

    script.scm - tan with cont-frac

--- script.scm
+++ script.scm
@@ -1,5 +1,6 @@
 (define (cont-frac n d k)
     (define (cont-frac-iter i ans)
+        ; (newline) (display "cont-frac-iter, i = ") (display i) (display ", num = ") (display (n i)) (display ", den = ") (display (d i)) (display ", ans = ") (display ans)
         (if (< i 1)
             ans
             (cont-frac-iter
@@ -7,16 +8,15 @@
                 (/ (n i) (+ (d i) ans)))))
     (cont-frac-iter k 0))
 
-(define (e-minus-2 k)
-    (cont-frac
-        (lambda (x) 1.0)
-        (lambda (x)
-            (if (= 2 (remainder x 3))
-                (* (/ 2.0 3) (+ x 1))
-                1.0))
-        k))
+(define (tan-cf x k)
+    (/
+        (cont-frac
+            (lambda (i) (* -1 (* x x)))
+            (lambda (i) (- (* 2 i) 1))
+            k)
+        (* -1 x)))
 
-(define (e-approximation k)
-    (+ 2 (e-minus-2 k)))
 
-(e-approximation 20)
+(define pi 3.14159265359)
+
+(tan-cf (/ pi 4) 10)
