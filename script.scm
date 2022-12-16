9f381c3e37fb1acb10f03d204397a4406273ceb7
Pranav Anurag <anuragpranav@gmail.com>
2022-12-16 18:52:50 +0530

    script.scm - repeated function

--- script.scm
+++ script.scm
@@ -1,4 +1,11 @@
 (define (compose f g)
   (lambda (x) (f (g x))))
 
-((compose square 1+) 6)
+(define (repeated f times)
+  (define (iter i)
+    (if (= i 1)
+      f
+      (compose f (iter (- i 1)))))
+  (iter times))
+
+((repeated square 2) 5)
