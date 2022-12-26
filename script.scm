4a8cbced06db81975317996de1ea16c27c371ad6
Pranav Anurag <anuragpranav@gmail.com>
2022-12-26 18:41:39 +0530

    script.scm - add-rat works

--- script.scm
+++ script.scm
@@ -1,8 +1,19 @@
-(define (make-rat n d) (cons n d))
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+    
 (define (numer x) (car x))
 (define (denom x) (cdr x))
 
-(define x (make-rat 12 13))
+(define (add-rat x y)
+  (make-rat (+
+      (* (numer x) (denom y))
+      (* (numer y) (denom x)))
+    (* (denom x) (denom y))))
+
+(define (equal-rat? x y)
+(= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
 
 (define (print-rat x)
     (newline)
@@ -10,6 +21,8 @@
     (display "/")
     (display (denom x)))
 
-(numer x)
-(denom x)
-(print-rat x)
+
+(define one-third (make-rat 1 3))
+
+(define sum (add-rat one-third one-third))
+(print-rat sum)
