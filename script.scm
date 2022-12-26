ef51a79abdbf232e2c72c3d54576983657cc3e9f
Pranav Anurag <anuragpranav@gmail.com>
2022-12-26 19:14:24 +0530

    script.scm - support positive and negative rational nums

--- script.scm
+++ script.scm
@@ -1,6 +1,15 @@
+(define (sign x) (if (= x 0) 0 (/ x (abs x))))
+
 (define (make-rat n d)
-  (let ((g (gcd n d)))
-    (cons (/ n g) (/ d g))))
+  (define (make-rat-sub n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (let ((fsign (* (sign n) (sign d))))
+    (set! n (* fsign (abs n)))
+    (set! d (abs d))
+    (newline) (display "updated values of n and d ") (display n) (display ", ") (display d) (display ", fsign = ") (display fsign)
+    (make-rat-sub n d)))
+
     
 (define (numer x) (car x))
 (define (denom x) (cdr x))
@@ -21,8 +30,12 @@
     (display "/")
     (display (denom x)))
 
+(define m-one-third (make-rat -1 3))
+
+(numer m-one-third)
+(denom m-one-third)
 
 (define one-third (make-rat 1 3))
 
-(define sum (add-rat one-third one-third))
+(define sum (add-rat one-third m-one-third))
 (print-rat sum)
