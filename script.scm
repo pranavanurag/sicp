8c5b624ebf4421ec022c3f0db11ec8d2b5a1fefe
Pranav Anurag <anuragpranav@gmail.com>
2022-12-26 19:23:19 +0530

    script.scm - basic point geometry

--- script.scm
+++ script.scm
@@ -1,40 +1,14 @@
-(define (sign x) (if (= x 0) 0 (/ x (abs x))))
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
 
-(define (make-rat n d)
-  (define (make-rat-sub n d)
-    (let ((g (gcd n d)))
-      (cons (/ n g) (/ d g))))
-  (let ((fsign (* (sign n) (sign d))))
-    (set! n (* fsign (abs n)))
-    (set! d (abs d))
-    (make-rat-sub n d)))
 
-    
-(define (numer x) (car x))
-(define (denom x) (cdr x))
-
-(define (add-rat x y)
-  (make-rat (+
-      (* (numer x) (denom y))
-      (* (numer y) (denom x)))
-    (* (denom x) (denom y))))
-
-(define (equal-rat? x y)
-(= (* (numer x) (denom y))
-     (* (numer y) (denom x))))
-
-(define (print-rat x)
-    (newline)
-    (display (numer x))
-    (display "/")
-    (display (denom x)))
-
-(define m-one-third (make-rat -1 3))
-
-(numer m-one-third)
-(denom m-one-third)
-
-(define one-third (make-rat 1 3))
-
-(define sum (add-rat one-third m-one-third))
-(print-rat sum)
+(define myp (make-point 12 13))
+(print-point myp)
