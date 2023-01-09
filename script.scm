5a08d6bce095adb05fe021a922f0584ff80a3da8
Pranav Anurag <anuragpranav@gmail.com>
2023-01-09 12:26:57 +0530

    script.scm - define width of interval

--- script.scm
+++ script.scm
@@ -1,14 +1,19 @@
 (define (make-interval a b) (cons a b))
 (define (lower-bound x) (car x))
 (define (upper-bound x) (cdr x))
+(define (width x) (/ (- (upper-bound x) (lower-bound x)) 2))
 
 (define (sub-interval x y)
   (make-interval
     (- (lower-bound x) (upper-bound y))
     (- (upper-bound x) (lower-bound y))))
 
-(define i1 (make-interval 1.3 1.4))
-(define i2 (make-interval 1.5 2.4))
+(define i1 (make-interval 1.6 2.4))
+(define i2 (make-interval 1.4 2.4))
 
-(lower-bound (sub-interval i1 i2))
-(upper-bound (sub-interval i1 i2))
+(define i3 (sub-interval i1 i2))
+(lower-bound i3)
+(upper-bound i3)
+(width i3)
+(width i1)
+(width i2)
