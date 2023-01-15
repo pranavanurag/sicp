9a0c8108972963e896e39c4f3ea267f150b3fdcb
Pranav Anurag <anuragpranav@gmail.com>
2023-01-16 02:49:57 +0530

    script.scm - complete 2.9

--- script.scm
+++ script.scm
@@ -8,12 +8,24 @@
     (- (lower-bound x) (upper-bound y))
     (- (upper-bound x) (lower-bound y))))
 
+
+(define (div-interval x y) (mul-interval
+   x
+   (make-interval (/ 1.0 (upper-bound y))
+                  (/ 1.0 (lower-bound y)))))
+
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+    (p2 (* (lower-bound x) (upper-bound y)))
+    (p3 (* (upper-bound x) (lower-bound y)))
+    (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
 (define i1 (make-interval 1.6 2.4))
 (define i2 (make-interval 1.4 2.4))
 
-(define i3 (sub-interval i1 i2))
-(lower-bound i3)
-(upper-bound i3)
-(width i3)
 (width i1)
 (width i2)
+(width (mul-interval i1 i2))
