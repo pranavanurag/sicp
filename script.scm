cb3d5fae74389a1c80e4f958030eb6ad06aac4bb
Pranav Anurag <anuragpranav@gmail.com>
2023-01-23 20:21:16 +0530

    whitespace

--- script.scm
+++ script.scm
@@ -5,9 +5,9 @@
 
 (define (make-center-width c w) (make-interval (- c w) (+ c w)))
 (define (center i)
-(/ (+ (lower-bound i) (upper-bound i)) 2))
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
 (define (width i)
-(/ (- (upper-bound i) (lower-bound i)) 2))
+  (/ (- (upper-bound i) (lower-bound i)) 2))
 
 
 (define (make-center-percent c p)
