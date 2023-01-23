55eaea9282bbf2c7a6e31848c738d33d1e273361
Pranav Anurag <anuragpranav@gmail.com>
2023-01-23 20:31:56 +0530

    script.scm - cleanup

--- script.scm
+++ script.scm
@@ -3,17 +3,19 @@
     (error "bro lmao")
     (cons a b)))
 
-(define (make-center-width c w) (make-interval (- c w) (+ c w)))
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
 (define (center i)
   (/ (+ (lower-bound i) (upper-bound i)) 2))
+
 (define (width i)
   (/ (- (upper-bound i) (lower-bound i)) 2))
 
 
 (define (make-center-percent c p)
-  (define (percent-as-decimal x)
-    (* 0.01 x))
   (make-center-width c (* (* 0.01 p) c)))
+
 (define (percent i)
   (* 100 (/ (width i) (center i))))
 
