c75bd6febec57c9fd63f66447d64cd85b5a926f2
Pranav Anurag <anuragpranav@gmail.com>
2023-02-10 19:37:44 +0530

    script.scm - code for 2.21

--- script.scm
+++ script.scm
@@ -1,24 +1,11 @@
-(define (reverse some-list)
-  (define (reverse-iter remaining ans)
-    (if (null? remaining)
-      ans
-      (reverse-iter (cdr remaining) (cons (car remaining) ans))))
-  (reverse-iter some-list '()))
+(define (square-list items)
+  (if (null? items)
+    '()
+    (cons
+      (square (car items))
+      (square-list (cdr items)))))
 
-  
-(define (first some-list)
-  (car some-list))
+(define (square-list2 items)
+  (map square items))
 
-(define (mod2 a) (remainder a 2))
-
-(define (same-parity . x)
-  (define (collect-parity-iter p curr ans)
-    (if (null? curr)
-      (reverse ans)
-      (if (= p (mod2 (car curr)))
-        (collect-parity-iter p (cdr curr) (cons (car curr) ans))
-        (collect-parity-iter p (cdr curr) ans))))
-  (let ((parity (mod2 (first x))))
-    (collect-parity-iter parity x '())))
-
-(same-parity 2 4 5 6 7 8 9 10 11 12 13)
+(square-list (list 1 2 34 4 5 5))
