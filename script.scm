fb76ac1b4d0eb93160212491a18f9d7c6407d584
Pranav Anurag <anuragpranav@gmail.com>
2023-02-13 21:28:20 +0530

    sciprt.scm - scratch for 2.24

--- script.scm
+++ script.scm
@@ -1,10 +1,25 @@
-(define (for-each f items)
-  ;(newline) (display "for-each invoked: ") (display items)
-  (if (null? items)
-    #t
-    ((for-each f (cdr items)) (f (car items)))))
+(define x (cons (list 1 2) (list 3 4)))
+
+(length x)
+
+(define (count-leaves tree)
+  (cond ((null? tree) 0)
+    ((not (pair? tree)) 1)
+    (else (+ (count-leaves (car tree)) (count-leaves (cdr tree))))))
+
+(count-leaves x)
+
+(define mytree (list 1 (list 2 (list 3 4))))
+(car mytree)
+(cdr mytree)
+(car (cdr mytree))
+(cdr (cdr mytree))
 
 
-(for-each
-  (lambda (x) (newline) (display x))
-  (list 57 321 88))
+(define mylist (list 1 2))
+(car mylist)
+(cdr mylist)
+
+(define mypair (cons 1 2))
+(car mypair)
+(cdr mypair)
