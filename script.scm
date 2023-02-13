fb1f018c4f72b79c5a0fc1bca16d987f12f8bd0f
Pranav Anurag <anuragpranav@gmail.com>
2023-02-13 21:32:14 +0530

    script.scm - 2.25 wip

--- script.scm
+++ script.scm
@@ -1,25 +1,4 @@
-(define x (cons (list 1 2) (list 3 4)))
+(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
+(define second (list (list 7)))
+(define third (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 
-(length x)
-
-(define (count-leaves tree)
-  (cond ((null? tree) 0)
-    ((not (pair? tree)) 1)
-    (else (+ (count-leaves (car tree)) (count-leaves (cdr tree))))))
-
-(count-leaves x)
-
-(define mytree (list 1 (list 2 (list 3 4))))
-(car mytree)
-(cdr mytree)
-(car (cdr mytree))
-(cdr (cdr mytree))
-
-
-(define mylist (list 1 2))
-(car mylist)
-(cdr mylist)
-
-(define mypair (cons 1 2))
-(car mypair)
-(cdr mypair)
