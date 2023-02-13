956d92c47aaf3e675c5aad4ab6d23280f82d477d
Pranav Anurag <anuragpranav@gmail.com>
2023-02-13 21:41:59 +0530

    script.scm - 2.26

--- script.scm
+++ script.scm
@@ -1,3 +1,6 @@
-(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
-(car (car (list (list 7))))
-(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))))))))))))))
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+(cons x y)
+(list x y)
