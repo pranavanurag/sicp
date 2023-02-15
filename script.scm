1c7b99f302c04629dc7e0aa934d2e759312bf7b6
Pranav Anurag <anuragpranav@gmail.com>
2023-02-15 12:53:35 +0530

    script.scm - code for 2.27

--- script.scm
+++ script.scm
@@ -1,6 +1,16 @@
-(define x (list 1 2 3))
-(define y (list 4 5 6))
+(define x (list (list 1 2) (list 3 4))) x
 
-(append x y)
-(cons x y)
-(list x y)
+(define (reverse some-list)
+  ;(newline) (display "reverse invoked with ") (display some-list)
+  (define (reverse-iter remaining ans)
+    (cond
+      ((null? remaining) ans)
+      ((not (pair? remaining)) remaining)
+      (else (reverse-iter
+        (cdr remaining)
+        (cons (reverse (car remaining)) ans)))))
+  (reverse-iter some-list '()))
+
+(reverse x)
+
+(reverse (list (list 3 4) (list 3 4 5 6 1 3)))
