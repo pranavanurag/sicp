a43fb90a4939e5b123b49cda8974d5c8fad078dc
Pranav Anurag <anuragpranav@gmail.com>
2023-02-19 19:01:03 +0530

    script.scm - code for 2.29 wip

--- script.scm
+++ script.scm
@@ -1,11 +1,26 @@
-(define (fringe tree)
-  (define (fringe-iter subtree)
-    (cond
-      ((null? subtree) '())
-      ((not (pair? subtree)) (list subtree))
-      (else (append (fringe (car tree)) (fringe (cdr tree))))))
-  (fringe-iter tree))
+(define (make-mobile left right) (list left right))
+(define (left-branch mobile) (car mobile))
+(define (right-branch mobile) (car (cdr mobile)))
 
-(define x (list (list 1 2) (list 3 4)))
-(fringe x)
-(fringe (list x (list 10 11 (list 12 13)) x))
+(define (make-branch length structure) (list length structure))
+(define (branch-length branch) (car branch))
+(define (branch-structure branch) (car (cdr branch)))
+
+
+(define (total-weight mobile)
+  (define (total-weight-iter submobile)
+  ;(newline) (display "total-weight-iter called with: ") (display submobile)
+    (cond
+      ((null? submobile) 0)
+      ((not (pair? submobile)) submobile)
+      (else (+
+        (total-weight-iter (branch-structure (left-branch submobile)))
+        (total-weight-iter (branch-structure (right-branch submobile)))))))
+  (total-weight-iter mobile))
+
+(define x
+  (make-mobile
+    (make-branch 1 (make-mobile (make-branch 2 4) (make-branch 1 1)))
+    (make-branch 2 3)))
+
+(total-weight x)
