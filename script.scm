1134b5d56e4d590e92157acb535c60dde238e7ea
Pranav Anurag <anuragpranav@gmail.com>
2023-02-21 22:26:05 +0530

    script.scm code for 2.30

--- script.scm
+++ script.scm
@@ -1,36 +1,8 @@
-(define (make-mobile left right) (cons left right)) 
-(define (left-branch mobile) (car mobile))
-(define (right-branch mobile) (cdr mobile))
+(define (square-tree tree)
+  (map (lambda (subtree)
+    (if (pair? subtree) (square-tree subtree) (* subtree subtree)))
+    tree))
 
-(define (make-branch length structure) (cons length structure))
-(define (branch-length branch) (car branch))
-(define (branch-structure branch) (cdr branch))
-
-
-(define (total-weight mobile)
-  (define (total-weight-iter submobile)
-    (cond
-      ((null? submobile) 0)
-      ((not (pair? submobile)) submobile)
-      (else (+
-        (total-weight-iter (branch-structure (left-branch submobile)))
-        (total-weight-iter (branch-structure (right-branch submobile)))))))
-  (total-weight-iter mobile))
-
-(define (balanced-mobile? mobile)
-  (newline) (display "balanced-mobile called with: ") (display mobile)
-  (define (torque branch)
-    (* (branch-length branch) (total-weight (branch-structure branch))))
-  (cond
-    ((null? mobile) #t)
-    ((not (pair? mobile)) #t)
-    (else (and
-      (= (torque (left-branch mobile)) (torque (right-branch mobile)))
-      (balanced-mobile? (branch-structure (left-branch mobile)))
-      (balanced-mobile? (branch-structure (right-branch mobile)))))))
-
-(define x (make-mobile
-  (make-branch 2 3)
-  (make-branch 1 (make-mobile (make-branch 1 3) (make-branch 1 3)))))
-
-(balanced-mobile? x)
+(define x (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+;(display x)
+(square-tree x)
