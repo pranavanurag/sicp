6c906c2a6d44df0e1be0256b38bb148fa04bb8f0
Pranav Anurag <anuragpranav@gmail.com>
2023-02-25 23:41:25 +0530

    script.scm - book notes

--- script.scm
+++ script.scm
@@ -1,8 +1,32 @@
-(define (subsets s)
-  (if (null? s)
-    (list '())
-    (let ((rest (subsets (cdr s))))
-      (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+(define (filter predicate sequence)
+    (cond
+        (null? sequence) '()
+        ((predicate (car sequence))
+            (cons
+                (car sequence)
+                (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
 
-(define x (list 1 2 3))
-(subsets x)
+(filter (lambda (x) (> x 0)) (list 1 -1 0 12 13 -100))
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (accumulate
+            op
+            (op initial (car sequence))
+            (cdr sequence))))
+
+(accumulate + 0 (list 1 2 34 5))
+
+
+(define (enumerate-interval low high)
+    (if (> low high)
+        '()
+        (cons low (enumerate-interval (+ low 1) high))))
+
+(enumerate-interval 0 9)
+
+
+
+
