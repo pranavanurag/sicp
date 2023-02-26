f17bef810560a01c02d0436bf385c70f5a2ee4f9
Pranav Anurag <anuragpranav@gmail.com>
2023-02-27 00:22:50 +0530

    script.scm - working accumulate

--- script.scm
+++ script.scm
@@ -1,16 +1,11 @@
 (define (accumulate op initial sequence)
     (if (null? sequence)
         initial
-        (accumulate
-            op
-            (op initial (car sequence))
-            (cdr sequence))))
-
-(define (accumulate2 op initial sequence)
-    (if (null? sequence)
-        initial
         (op
             (car sequence)
-            (accumulate2 op intial (cdr sequence)))))
+            (accumulate op initial (cdr sequence)))))
 
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(accumulate cons y x)
 
