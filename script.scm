1150172d4cf52d187bd7195331ae31e77ea24b3b
Pranav Anurag <anuragpranav@gmail.com>
2023-02-27 00:17:55 +0530

    script.scm - wip debug accumulate

--- script.scm
+++ script.scm
@@ -1,14 +1,3 @@
-(define (filter predicate sequence)
-    (cond
-        (null? sequence) '()
-        ((predicate (car sequence))
-            (cons
-                (car sequence)
-                (filter predicate (cdr sequence))))
-        (else (filter predicate (cdr sequence)))))
-
-(filter (lambda (x) (> x 0)) (list 1 -1 0 12 13 -100))
-
 (define (accumulate op initial sequence)
     (if (null? sequence)
         initial
@@ -17,16 +6,11 @@
             (op initial (car sequence))
             (cdr sequence))))
 
-(accumulate + 0 (list 1 2 34 5))
-
-
-(define (enumerate-interval low high)
-    (if (> low high)
-        '()
-        (cons low (enumerate-interval (+ low 1) high))))
-
-(enumerate-interval 0 9)
-
-
+(define (accumulate2 op initial sequence)
+    (if (null? sequence)
+        initial
+        (op
+            (car sequence)
+            (accumulate2 op intial (cdr sequence)))))
 
 
