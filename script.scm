87ff745d3f449b3485b54f9a9e40a1beb25c257a
Pranav Anurag <anuragpranav@gmail.com>
2023-02-27 00:32:49 +0530

    script.scm - code for 2.33

--- script.scm
+++ script.scm
@@ -7,5 +7,20 @@
 
 (define x (list 1 2 3))
 (define y (list 4 5 6))
-(accumulate cons y x)
+(define (append2 x y)
+    (accumulate cons y x))
 
+(define (map2 p sequence)
+    (accumulate
+        (lambda (current rest) (cons (p current) rest))
+        '()
+        sequence))
+
+
+(map2 square (list 1 2 3 4))
+
+
+(define (length sequence)
+    (accumulate (lambda (current rest) (+ 1 rest)) 0 sequence))
+
+(length (list 1 2 3 5 123 5 917 1977 20 2974))
