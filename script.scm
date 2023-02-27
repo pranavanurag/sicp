720122dd3b275ab53a092177617bdc412f9f6b2c
Pranav Anurag <anuragpranav@gmail.com>
2023-02-28 01:43:03 +0530

    script.scm - code for 2.36

--- script.scm
+++ script.scm
@@ -5,47 +5,12 @@
       (car sequence)
       (accumulate op initial (cdr sequence)))))
 
-(define (count-leaves x)
-  (cond
-    ((null? x) 0)
-    ((not (pair? x)) 1)
-    (else (+ (count-leaves (car x)) (count-leaves (cdr x))))))
 
-(define (flatten-tree x)
-  (newline) (display "flatten-tree invoked with: ") (display x)
-  (cond
-    ((null? x) '())
-    ((not (pair? x)) (list x))
-    (else (append (flatten-tree (car x)) (flatten-tree (cdr x))))))
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    '()
+    (cons
+      (accumulate op init (map (lambda (x) (car x)) seqs))
+      (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
 
-(define t1 (list 1 (list 2 3)))
-(define t2 (list 1 (list 2 3) (list 4 5 (list 6 (list 7) 8))))
-
-(count-leaves t1)
-
-(map flatten-tree t2)
-
-(count-leaves t2)
-
-
-(pair? (list 7))
-(car (list 7))
-(cdr (list 7))
-
-(append (list 7) '())
-
-
-(define (count-leaves-2 x)
-  (newline) (display "count-leaves-2 invoked with: ") (display x)
-  (accumulate
-    +
-    0
-    (map
-      (lambda (node)
-        (cond
-          ((null? node) 0)
-          ((not (pair? node)) 1)
-          (else (count-leaves-2 node))))
-      x)))
-
-(count-leaves-2 t2)
+(accumulate-n + 0 (list (list 1 2 3 4) (list 5 6 7 8) (list 9 10 11 12) (list 13 14 15 16)))
