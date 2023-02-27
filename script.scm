b1c4f47d26d097eeb63db366cc2e05a69c563d0b
Pranav Anurag <anuragpranav@gmail.com>
2023-02-27 21:13:02 +0530

    script.scm - code for 2.34

--- script.scm
+++ script.scm
@@ -1,26 +1,15 @@
 (define (accumulate op initial sequence)
-    (if (null? sequence)
-        initial
-        (op
-            (car sequence)
-            (accumulate op initial (cdr sequence)))))
+  (if (null? sequence)
+    initial
+    (op
+      (car sequence)
+      (accumulate op initial (cdr sequence)))))
 
-(define x (list 1 2 3))
-(define y (list 4 5 6))
-(define (append2 x y)
-    (accumulate cons y x))
+(define (horner-eval x coeffecient-sequence)
+  (accumulate
+    (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)))
+    0
+    coeffecient-sequence))
 
-(define (map2 p sequence)
-    (accumulate
-        (lambda (current rest) (cons (p current) rest))
-        '()
-        sequence))
-
-
-(map2 square (list 1 2 3 4))
-
-
-(define (length sequence)
-    (accumulate (lambda (current rest) (+ 1 rest)) 0 sequence))
-
-(length (list 1 2 3 5 123 5 917 1977 20 2974))
+(horner-eval 2 (list 4 3 2 1))
+(horner-eval 2 (list 1 3 0 5 0 1))
