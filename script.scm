18b74488fb869a00b5a15cc49b4503c834fc17ad
Pranav Anurag <anuragpranav@gmail.com>
2023-02-28 01:25:49 +0530

    script.scm - code for 2.35

--- script.scm
+++ script.scm
@@ -5,11 +5,47 @@
       (car sequence)
       (accumulate op initial (cdr sequence)))))
 
-(define (horner-eval x coeffecient-sequence)
-  (accumulate
-    (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)))
-    0
-    coeffecient-sequence))
+(define (count-leaves x)
+  (cond
+    ((null? x) 0)
+    ((not (pair? x)) 1)
+    (else (+ (count-leaves (car x)) (count-leaves (cdr x))))))
 
-(horner-eval 2 (list 4 3 2 1))
-(horner-eval 2 (list 1 3 0 5 0 1))
+(define (flatten-tree x)
+  (newline) (display "flatten-tree invoked with: ") (display x)
+  (cond
+    ((null? x) '())
+    ((not (pair? x)) (list x))
+    (else (append (flatten-tree (car x)) (flatten-tree (cdr x))))))
+
+(define t1 (list 1 (list 2 3)))
+(define t2 (list 1 (list 2 3) (list 4 5 (list 6 (list 7) 8))))
+
+(count-leaves t1)
+
+(map flatten-tree t2)
+
+(count-leaves t2)
+
+
+(pair? (list 7))
+(car (list 7))
+(cdr (list 7))
+
+(append (list 7) '())
+
+
+(define (count-leaves-2 x)
+  (newline) (display "count-leaves-2 invoked with: ") (display x)
+  (accumulate
+    +
+    0
+    (map
+      (lambda (node)
+        (cond
+          ((null? node) 0)
+          ((not (pair? node)) 1)
+          (else (count-leaves-2 node))))
+      x)))
+
+(count-leaves-2 t2)
