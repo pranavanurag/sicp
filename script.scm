822df90ad0cbaad40aecbbd4c16111e0bdae1291
Pranav Anurag <anuragpranav@gmail.com>
2023-02-28 23:01:32 +0530

    script.scm - code for 2.38

--- script.scm
+++ script.scm
@@ -1,40 +1,51 @@
 (define (accumulate op initial sequence)
+  ;(newline) (display "fold-right iter invoked with: ") (display initial) (display ", ") (display sequence)
   (if (null? sequence)
     initial
     (op
       (car sequence)
       (accumulate op initial (cdr sequence)))))
 
+(define (fold-right op initial sequence) (accumulate op initial sequence))
 
-(define (accumulate-n op init seqs)
-  (if (null? (car seqs))
-    '()
-    (cons
-      (accumulate op init (map (lambda (x) (car x)) seqs))
-      (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    ;(newline) (display "fold-left iter invoked with: ") (display result) (display ", ") (display rest)
+    (if (null? rest)
+      result
+      (iter
+        (op result (car rest))
+        (cdr rest))))
+  (iter initial sequence))
 
-(define (dot-product v w)
-  (accumulate + 0 (map * v w)))
+(fold-left + 0 (list 1 2 3 4))
+(fold-right + 0 (list 1 2 3 4))
 
-(dot-product (list 1 2 3 4) (list 1 2 3 4))
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
 
-(define (matrix-*-vector m v)
-  (map (lambda (row) (dot-product row v)) m))
+(fold-right list '() (list 1 2 3))
+(fold-left list '() (list 1 2 3))
 
-;(matrix-*-vector (list (list 1 2 3 4) (list 5 6 7 8)) (list 1 2 3 4))
+(fold-right cons '() (list 1 2 3))
+(fold-left cons '() (list 1 2 3))
 
-(define (transpose m)
- (accumulate-n cons '() m))
+(fold-right * 1 (list 1 2 3))
+(fold-left * 1 (list 1 2 3))
 
-;(transpose (list (list 1 2 3 4) (list 5 6 7 8)))
 
-(define (matrix-*-matrix m n)
-  (let ((cols (transpose n)))
-    (map (lambda (m-row) (matrix-*-vector cols m-row)) m)))
 
-(matrix-*-matrix (list (list 1 2 3 4) (list 5 6 7 8)) (list (list 1 1) (list 1 1) (list 1 1) (list 1 1)))
+(fold-right + 1 (list 1 2 3))
+(fold-left + 1 (list 1 2 3))
 
-(define a (list (list 34 3 -9 5) (list 12 -2 6 1)))
-(define b (list (list 1 2) (list 3 -4) (list 2 5) (list -3 -17)))
 
-(matrix-*-matrix a b)
+(fold-right - 1 (list 1 2 3))
+(fold-left - 1 (list 1 2 3))
+
+
+(fold-right append '() (list (list 1 2 3) (list 4 5 6)))
+(fold-left append '() (list (list 1 2 3) (list 4 5 6)))
+
+
+(fold-right (lambda (x y) (+ 1 (* x y))) 1 (list 1 2 3))
+(fold-left (lambda (x y) (+ 1 (* x y))) 1 (list 1 2 3))
