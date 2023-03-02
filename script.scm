e3efe0d790799c633585c80ba914f73fbd1baf4b
Pranav Anurag <anuragpranav@gmail.com>
2023-03-03 00:14:05 +0530

    script.scm - code for 2.40

--- script.scm
+++ script.scm
@@ -1,28 +1,65 @@
+(define (sq x) (* x x))
+(define (even n) (= 0 (remainder n 2)))
+
+(define (fast-exp-mod base power mod)
+  (if (= power 0)
+    (remainder 1 mod)
+    (if (even power)
+      (fast-exp-mod (remainder (sq base) mod) (/ power 2) mod)
+      (remainder (* base (fast-exp-mod (sq base) (/ (- power 1) 2) mod)) mod))))
+
+(define (fermat-test n)
+  (define (try-it a) (= (fast-exp-mod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond
+    ((= times 0) true)
+    ((fermat-test n) (fast-prime? n (- times 1)))
+    (else false)))
+
+(define (prime? n) (fast-prime? n 50))
+
+
+
+
+
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+        ((predicate (car sequence))
+          (cons
+            (car sequence)
+            (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
 (define (accumulate op initial sequence)
-  ;(newline) (display "fold-right iter invoked with: ") (display initial) (display ", ") (display sequence)
   (if (null? sequence)
     initial
     (op
       (car sequence)
       (accumulate op initial (cdr sequence)))))
 
-(define (fold-right op initial sequence) (accumulate op initial sequence))
+(define (enumerate-interval low high)
+  (if (> low high)
+    '()
+    (cons low (enumerate-interval (+ low 1) high))))
 
-(define (fold-left op initial sequence)
-  (define (iter result rest)
-    ;(newline) (display "fold-left iter invoked with: ") (display result) (display ", ") (display rest)
-    (if (null? rest)
-      result
-      (iter
-        (op result (car rest))
-        (cdr rest))))
-  (iter initial sequence))
+(define (unique-pairs n)
+  (flatmap
+    (lambda (i)
+        (map
+          (lambda (j) (list i j))
+          (enumerate-interval 1 (- i 1))))
+    (enumerate-interval 1 n)))
 
-(define (reverse-r sequence)
-  (fold-right (lambda (current-element rest-result) (append rest-result (list current-element))) '() sequence))
+(define (flatmap proc seq) (accumulate append '() (map proc seq)))
 
-(define (reverse-l sequence)
-  (fold-left (lambda (current-element rest-result) (cons rest-result current-element)) '() sequence))
+(define (prime-sum? pair) (prime? (+ (car pair) (cadr pair))))
 
-(reverse-r (list 1 2 3 4))
-(reverse-l (list 1 2 3 4))
+(define (make-pair-sum pair) (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs 100)
