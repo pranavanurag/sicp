659aba9e0575293603039d7ad3f090a36364e8a2
Pranav Anurag <anuragpranav@gmail.com>
2023-03-12 19:09:48 +0530

    script.scm - code for 2.42

--- script.scm
+++ script.scm
@@ -1,38 +1,3 @@
-(define (sq x) (* x x))
-(define (even n) (= 0 (remainder n 2)))
-
-(define (fast-exp-mod base power mod)
-  (if (= power 0)
-    (remainder 1 mod)
-    (if (even power)
-      (fast-exp-mod (remainder (sq base) mod) (/ power 2) mod)
-      (remainder (* base (fast-exp-mod (sq base) (/ (- power 1) 2) mod)) mod))))
-
-(define (fermat-test n)
-  (define (try-it a) (= (fast-exp-mod a n n) a))
-  (try-it (+ 1 (random (- n 1)))))
-
-(define (fast-prime? n times)
-  (cond
-    ((= times 0) true)
-    ((fermat-test n) (fast-prime? n (- times 1)))
-    (else false)))
-
-(define (prime? n) (fast-prime? n 50))
-
-
-
-
-
-
-(define (filter predicate sequence)
-  (cond ((null? sequence) '())
-        ((predicate (car sequence))
-          (cons
-            (car sequence)
-            (filter predicate (cdr sequence))))
-        (else (filter predicate (cdr sequence)))))
-
 (define (accumulate op initial sequence)
   (if (null? sequence)
     initial
@@ -45,29 +10,43 @@
     '()
     (cons low (enumerate-interval (+ low 1) high))))
 
-(define (unique-pairs n)
-  (flatmap
-    (lambda (i)
-        (map
-          (lambda (j) (list i j))
-          (enumerate-interval 1 (- i 1))))
-    (enumerate-interval 1 n)))
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
 
-(define (flatmap proc seq) (accumulate append '() (map proc seq)))
+(define empty-board '())
 
-(define (make-pair-sum pair) (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+(define (adjoin-position new-queen-pos k other-queens)
+  (cons new-queen-pos other-queens))
 
-(define (sum-to? s) (lambda (triplet) (= s (+ (car triplet) (cadr triplet) (caddr triplet)))))
 
-(define (unique-triplets n)
-  (flatmap
-    (lambda (i)
-      (map
-        (lambda (pair) (append (list i) pair))
-        (unique-pairs (- i 1))))
-    (enumerate-interval 1 n)))
+; positions are lists of size 'k', where each element lies between 0 and '(board-size - 1)'
+; only the new queen at (car position) needs to be verified to be safe
+(define (safe? k positions)
+  (let ((new-queen (car positions)))
+    (define (safe-iter col-distance remaining-queens)
+      (if (null? remaining-queens)
+        #t
+        (cond
+          ((= (car remaining-queens) new-queen) #f) ;same row
+          ((= col-distance (abs (- new-queen (car remaining-queens)))) #f)  ;same diagonal
+          (else (safe-iter (+ col-distance 1) (cdr remaining-queens)))))) ;check other queens
+    (safe-iter 1 (cdr positions))))
 
-(define (find-sum-triplets sum n)
-  (filter (sum-to? sum) (unique-triplets n)))
+(safe? 3 (list 2 2 0))
 
-(find-sum-triplets 10 8)
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+      (list empty-board)
+      (filter
+        (lambda (positions) (safe? k positions))
+        (flatmap
+          (lambda (rest-of-queens)
+            (map
+              (lambda (new-row) (adjoin-position new-row k rest-of-queens))
+              (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+
+(length (queens 8))
