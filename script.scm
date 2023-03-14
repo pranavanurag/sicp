fbdde1fe378627551a55457e956b5c5aee6e7c7f
Pranav Anurag <anuragpranav@gmail.com>
2023-03-14 23:36:29 +0530

    script.scm - code for 2.45

--- script.scm
+++ script.scm
@@ -1,5 +1,6 @@
-(define (up-split painter n)
-  (if (= n 0)
-    painter
-    (let ((smaller (up-split painter (- n 1))))
-      (below painter (beside smaller smaller)))))
+(define (split t1 t2)
+  (lambda (painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller ((split t1 t2) painter (- n 1))))
+        (t1 painter (t2 smaller smaller))))))
