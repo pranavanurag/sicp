532903a02dc2239f5b02b074106ed343397d4bbf
Pranav Anurag <anuragpranav@gmail.com>
2023-03-21 23:02:14 +0530

    script.scm - wip 2.51

--- script.scm
+++ script.scm
@@ -119,4 +119,23 @@
     (make-vect 0 1)
     (make-vect 0 0)
     (make-vect 1 1)))
-    
+
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0 0.5)))
+    (let
+      ((paint-top
+        (transform-painter
+          painter2
+          (make-vect 0 0)
+          (make-vect 1 0)
+          split-point))
+      (paint-bottom
+        (transform-painter
+          painter1
+          split-point
+          (make-vect 1 0.5)
+          (make-vect 0 1))))
+      (lambda (frame) (paint-top frame) (paint-bottom frame)))))
+
+
