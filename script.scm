c05eb313d25f442c7d3e87ebbcd6b96e2a5865c5
Pranav Anurag <anuragpranav@gmail.com>
2023-03-24 18:34:16 +0530

    script.scm - code for 2.55

--- script.scm
+++ script.scm
@@ -1,8 +1,3 @@
-(define (my-equal? seq1 seq2)
-  (cond
-    ((and (null? seq1) (null? seq2)) #t)
-    ((or (null? seq1) (null? seq2)) #f)
-    ((eq? (car seq1) (car seq2)) (my-equal? (cdr seq1) (cdr seq2)))
-    (else #f)))
-
-(my-equal? (list 1 2 3) (list 1 3))
+(quote abracadabra)
+'abracadabra
+(car (quote (quote (abracadabra))))
