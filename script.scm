83f323dd7860004b650ac4439d49e9d48f072166
Pranav Anurag <anuragpranav@gmail.com>
2023-04-03 00:34:27 +0530

    script.scm - code for 2.59

--- script.scm
+++ script.scm
@@ -1,112 +1,34 @@
-(define (accumulate op initial sequence)
-  (if (null? sequence)
-    initial
-    (op
-      (car sequence)
-      (accumulate op initial (cdr sequence)))))
 
-(define (=number? exp num) (and (number? exp) (= exp num)))
-
-(define (variable? x) (symbol? x))
-
-(define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
-
-
-(define (make-sum . parts)
-  (let
-    ((num-sum (accumulate + 0 (filter number? parts)))
-      (others (filter (lambda (x) (not (number? x))) parts)))
-    ;(newline) (display "others = ") (display others) (display ", num-sum = ") (display num-sum)
-    (cond
-      ((and (null? others) (= num-sum 0)) 0)
-      ((= 0 num-sum) (cons '+ others))
-      ((null? others) num-sum)
-      (else (newline) (append (cons '+ others) (list num-sum))))))
-
-(define (sum? x) (and (pair? x) (eq? (car x) '+)))
-
-(define (addend s) (cadr s))
-
-(define (augend s)
-  (if (pair? s)
-    (if (= (length s) 2)
-      0
-      (if (= (length s) 3)
-        (caddr s)
-        (cons '+ (cddr s))))
-    s))
-  
-
-
-(define (make-product . parts)
-  (let
-    ((num-product (accumulate * 1 (filter number? parts)))
-      (others (filter (lambda (x) (not (number? x))) parts)))
-    ;(newline) (display "others = ") (display others) (display ", num-product = ") (display num-product)
-    (cond
-      ((= num-product 0) 0)
-      ((= 1 num-product) (cons '* others))
-      ((null? others) num-product)
-      (else (append (cons '* others) (list num-product))))))
-
-(define (product? x) (and (pair? x) (eq? (car x) '*)))
-
-(define (multiplier p) (cadr p))
-
-(define (multiplicand p)
-  (if (pair? p)
-    (if (= (length p) 2)
-      1
-      (if (= (length p) 3)
-        (caddr p)
-        (cons '* (cddr p))))
-    p))
-
-
-
-(define (make-exponentiation base exponent)
+(define (element-of-set? x set)
   (cond
-    ((and (number? exponent) (= exponent 0)) 1)
-    ((and (number? exponent)(= exponent 1)) base)
-    ((and (number? base )(= base 0)) 0);unless exponent = 0
-    (else (list '** base exponent))))
-
-;return true if expression = f(x)^number only
-(define (exponentiation? expression var)
-  (and
-    (pair? expression)
-    (eq? '** (car expression))
-    (number? (exponent expression))))
-
-(define (base e) (cadr e))
-
-(define (exponent e) (caddr e))
+    ((null? set) false)
+    ((equal? x (car set)) true)
+    (else (element-of-set? x (cdr set)))))
 
 
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
 
-(define (deriv exp var)
-  ;(newline) (display "deriv called with: ") (display exp)
+(define (intersection-set set1 set2)
   (cond
-    ((number? exp) 0)
-    ((variable? exp)
-      (if (same-variable? exp var) 1 0))
-    ((sum? exp)
-      (make-sum
-        (deriv (addend exp) var)
-        (deriv (augend exp) var)))
-    ((product? exp)
-      (make-sum
-        (make-product (multiplier exp) (deriv (multiplicand exp) var))
-        (make-product (deriv (multiplier exp) var) (multiplicand exp))))
-    ((exponentiation? exp var)
-      (make-product
-        (exponent exp)
-        (make-product
-          (make-exponentiation (base exp) (- (exponent exp) 1))
-          (deriv (base exp) var))))
-    (else (error "unknown expression type: DERIV" exp))))
+    ((or (null? set1) (null? set2)) '())
+    ((element-of-set? (car set1) set2)
+        (cons (car set1) (intersection-set (cdr set1) set2)))
+    (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond
+    ((null? set1) set2)
+    ((null? set2) set1)
+    (else (adjoin-set (car set1) (union-set (cdr set1) set2)))))
 
 
-(define expr '(* x x x))
-(deriv expr 'x)
+(define a (adjoin-set 1 (list 1 2 3)))
+(define b (adjoin-set 2 (list 4 5 6)))
+(display a)
+(display b)
+(intersection-set a b)
+
+(union-set a b)
