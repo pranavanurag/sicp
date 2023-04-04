36c473febfb81b7d6578d72e8b71ea901c468f43
Pranav Anurag <anuragpranav@gmail.com>
2023-04-04 15:39:35 +0530

    script.scm - wip bintree as sets

--- script.scm
+++ script.scm
@@ -1,45 +1,25 @@
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+
+(define (make-leaf value) (make-tree value '() '()))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
 (define (element-of-set? x set)
+  (newline) (display "element-of-set? invoked with x = ") (display x) (display ", and set = ") (display set)
   (cond
     ((null? set) false)
-    ((= x (car set)) true)
-    ((< x (car set)) false)
-    (else (element-of-set? x (cdr set)))))
+    ((= x (entry set)) true)
+    ((< x (entry set)) (element-of-set? x (left-branch set)))
+    ((> x (entry set)) (element-of-set? x (right-branch set)))))
 
-(define (adjoin-set x set)
-  (cond
-    ((null? set) (list x))
-    ((> (car set) x) (cons x set))
-    ((= (car set) x) set)
-    (else (cons (car set) (adjoin-set x (cdr set))))))
+(define mytree
+  (make-tree
+    7
+    (make-tree 3 (make-leaf 1) (make-leaf 5))
+    (make-tree 9 '() (make-leaf 11))))
 
-(define myset (list 1 2 3 4 5))
-(adjoin-set 2.5 myset)
-
-(define (intersection-set set1 set2)
-  (if (or (null? set1) (null? set2))
-    '()
-    (let ((x1 (car set1)) (x2 (car set2)))
-      (cond
-        ((= x1 x2) (cons x1 (intersection-set (cdr set1) (cdr set2))))
-        ((< x1 x2) (intersection-set (cdr set1) set2))
-        ((< x2 x1) (intersection-set set1 (cdr set2)))))))
-
-
-(define myset2 (list 4 5 6))
-
-
-(intersection-set myset (adjoin-set 1 myset2))
-
-
-(define (union-set set1 set2)
-  (cond
-    ((null? set1) set2)
-    ((null? set2) set1)
-    (else
-      (let ((x1 (car set1)) (x2 (car set2)))
-       (cond
-        ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
-        ((> x1 x2) (cons x2 (union-set set1 (cdr set2))))
-        ((< x1 x2) (cons x1 (union-set (cdr set1) set2))))))))
-
-(union-set (list 1 2.5 3 4) (list 2 3 4 7))
+(display mytree)
+(element-of-set? 5 mytree)
