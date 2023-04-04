7e9001f3f931cc8c8fdc089e45179a40959cade1
Pranav Anurag <anuragpranav@gmail.com>
2023-04-04 16:05:44 +0530

    script.scm - wip 2.63

--- script.scm
+++ script.scm
@@ -15,11 +15,41 @@
     ((< x (entry set)) (element-of-set? x (left-branch set)))
     ((> x (entry set)) (element-of-set? x (right-branch set)))))
 
+
+(define (adjoin-set x set)
+  (cond
+    ((null? set) (make-leaf x))
+    ((= x (entry set)) set)
+    ((> x (entry set))
+      (make-tree (entry set) (left-branch set) (adjoin-set x (right-branch set))))
+    ((< x (entry set))
+      (make-tree (entry set) (adjoin-set x (left-branch set)) (right-branch set)))))
+
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append
+      (tree->list-1 (left-branch tree))
+      (cons (entry tree) (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list
+          (left-branch tree)
+          (cons
+            (entry tree)
+            (copy-to-list (right-branch tree) result-list)))))
+  (copy-to-list tree '()))
+
+
 (define mytree
   (make-tree
     7
     (make-tree 3 (make-leaf 1) (make-leaf 5))
     (make-tree 9 '() (make-leaf 11))))
 
-(display mytree)
-(element-of-set? 5 mytree)
+(tree->list-1 mytree2)
+(tree->list-2 mytree2)
