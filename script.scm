055954b0e66795ac0b18667a1bce1a2d11d7bdf9
Pranav Anurag <anuragpranav@gmail.com>
2023-04-04 20:34:56 +0530

    script.scm - code for 2.64

--- script.scm
+++ script.scm
@@ -83,6 +83,7 @@ rep3
   (/ (- dividend (remainder dividend divisor)) divisor))
 
 (define (partial-tree elts n)
+  (newline) (display "partial-tree invoked with elts = ") (display elts) (display ", n = ") (display n)
   (if (= n 0)
     (cons '() elts)
     (let ((left-size (qoutient (- n 1) 2)))
@@ -105,3 +106,4 @@ rep3
 
 
 (list->tree (list 1 2 4 5 6))
+(list->tree (list 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51))
