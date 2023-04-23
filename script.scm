5f2e9e8a971d36e00ff7c43ec15fcc2a9a172b45
Pranav Anurag <anuragpranav@gmail.com>
2023-04-23 23:19:21 +0530

    script.scm - code for 3.2

--- script.scm
+++ script.scm
@@ -1,47 +1,22 @@
-;we dont want to translate all the rules for our system into language
-;our system can understand our rules as is
 
 
-;all differentiation rules are like this:
+(define (make-monitored f)
+  (define counter 0)
+  (lambda (prompt)
+    (cond
+      ((equal? prompt 'how-many-calls?) counter)
+      ((equal? prompt 'reset-counter) (set! counter 0))
+      (else
+        (begin
+          (set! counter (+ 1 counter))
+          (f prompt))))))
 
-         (rules)
-pattern ----------> skeleton (with placeholders to create the answer)
+(define m (make-monitored (lambda (x) (* x x))))
 
-   |
-   | (match)
-   |
-   .
-set of expressions
-
-
-pattern match
-foo - matches exactly foo
-(f a b) - matches a list whose first element is f, second element is a, third element is b
-(? x) - matches anything that is, and calls it x
-(?c x) - matches constants, calls it x
-(?v x) - matches variables, calls it x
-
-
-skeletons
-foo - instantiates exactly itself
-(f a b) - instantiates to a 3-list which are the **result of instantiating** each of f, a, b
-(:x) - instantitates to the value of x as in the matched pattern (called x during pattern match)
- 
-
-
- (define dsimp (simplifier deriv-rules))
- this procedure simplifier can now take as its input deriv-rules
- and simplify them -> feed it to the system written
- this procedure is generic, such that a different system, 
-  for example one to evaulate algebraic expressions
-
-with all these elements - we would have created a language to solve
-  all problems in this class of programs of pattern matching and substitution
-  so we can make up whatever kind of rules we like
-
-=> (dsimp '(dd (+ x y) x))
-; will evaluate to
-; (+ 1 0)
-
-
- 
+(m 10)
+(m 10)
+(m 10)
+(m 'reset-counter)
+(m 10)
+(m 10)
+(m 'how-many-calls?)
