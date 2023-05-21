9adc12d13f465c8c719d7bfaa674271fe7d8ca05
Pranav Anurag <anuragpranav@gmail.com>
2023-05-21 15:44:20 +0530

    script.scm - wip monte carlo example

--- script.scm
+++ script.scm
@@ -1,46 +1,28 @@
-(define (make-account balance password)
-  (define failed-attempts 0)
+x2 = (rand-update 1)
+x3 = (rand-update x2)
 
-  (define (withdraw amount)
-    (if (>= balance amount)
-      (begin (set! balance (- balance amount)) balance)
-      "Insufficient funds"))
+(define rand
+  (let ((x random-int))
+    (lambda () (set! x (rand-update x)) x)))
 
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carle trials cesaro-test))))
 
-  (define call-the-cops
-    "Any gangster will tell you that the strain and anxiety of busting a safe are greatly diminished if you've a look-out man ready at any moment to say 'Cheese it, the cops!'")
 
-  (define (deny amount)
-      (if (>= failed-attempts 7)
-        call-the-cops
-        (begin
-          (newline) (display "amount invoked, failed attempts = ") (display failed-attempts)
-          (set! failed-attempts (+ 1 failed-attempts))
-          "Incorrect password")))
-    
-  (define (dispatch p m)
-    (if (eq? password p)
-      (begin
-        (set! failed-attempts 0)
-        (cond
-          ((eq? m 'withdraw) withdraw)
-          ((eq? m 'deposit) deposit)
-          (else (error "Unknown request: MAKE-ACCOUNT" m))))
-      deny))
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond
+      ((= trials-remaining 0) (/ trials-passed trials))
+      ((experiment) (iter (-trials-remaining 1) (+ trials-passed 1)));HAHA GOOD LUCK DEFINING AN EXPERIMENT WITH NO ARGUMENTS
+      (else (iter trials-remaining trials-passed))))
+    (iter trials))
 
-  dispatch)
+(define (estimate-pi trials)
+  (sqrt (/ 6 (random-gcd-test trials random-int))))
+    ;it's annoying to carry this random-int in any kind of monte carlo simulation i want to do
+  ;this should be a detail in the black box "generating random numbers", not leak out
 
-(define myaccount (make-account 100 'king-crimson))
-((myaccount 'king-crimson 'withdraw) 70)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
-((myaccount 'aphex-crimson 'withdraw) 100)
+
+(define (random-gcd-test trials random-int)
+  (define iter trials-remaining trials passed x)
+    ())
