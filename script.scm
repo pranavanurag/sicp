2ec9bdace58cc4454cc46c49f89cc6057a9d1549
Pranav Anurag <anuragpranav@gmail.com>
2023-05-27 23:09:10 +0530

    script.scm - i think my LCG might be shit

--- script.scm
+++ script.scm
@@ -9,16 +9,18 @@
 (define random-init 41232) ; Seed value
 (define rand-update (LCG a c m))
 
-(define (estimate-pi trials) (sqrt (/ 6 (random-gcd-test trials random-init))))
+(define rand (let ((x random-init)) (lambda () (set! x (rand-update x)) x)))
 
-(define (random-gcd-test trials initial-x)
-  (define (iter trials-remaining trials-passed x)
-    (let ((x1 (rand-update x))) (let ((x2 (rand-update x1)))
-      (cond ((= trials-remaining 0) (/ trials-passed trials))
-            ((= (gcd x1 x2) 1)
-              (iter (- trials-remaining 1) (+ trials-passed 1) x2))
-            (else
-              (iter (- trials-remaining 1) trials-passed x2))))))
-  (iter trials 0 initial-x))
+(define (estimate-pi trials) (sqrt (/ 6 (monte-carlo trials cesaro-test))))
 
-(estimate-pi 1000000)
+(define (cesaro-test) (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0) (/ trials-passed trials))
+          ((experiment) (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else (iter (- trials-remaining 1) trials-passed))))
+    (iter trials 0))
+
+
+(estimate-pi 10000)
