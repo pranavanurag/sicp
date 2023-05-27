9ad2ca4faadc71a3b8ca87b7787551ad675b20e9
Pranav Anurag <anuragpranav@gmail.com>
2023-05-27 23:06:20 +0530

    script.scm - pi is 2.7212

--- script.scm
+++ script.scm
@@ -2,21 +2,23 @@
   (lambda (x)
     (modulo (+ (* a x) c) m)))
 
-; Define the LCG parameters
 (define a 1664525)
 (define c 1013904223)
 (define m (expt 2 32))
 
-; Define random-init and rand-update functions
-(define random-init 42) ; Seed value
+(define random-init 41232) ; Seed value
 (define rand-update (LCG a c m))
 
-; Define the rand procedure
-(define rand (let ((x random-init))
-  (lambda ()
-    (set! x (rand-update x))
-    x)))
+(define (estimate-pi trials) (sqrt (/ 6 (random-gcd-test trials random-init))))
 
-; Usage example
-(display (rand)) (newline) ; Print a random number
-(display (rand)) (newline) ; Print another random number
+(define (random-gcd-test trials initial-x)
+  (define (iter trials-remaining trials-passed x)
+    (let ((x1 (rand-update x))) (let ((x2 (rand-update x1)))
+      (cond ((= trials-remaining 0) (/ trials-passed trials))
+            ((= (gcd x1 x2) 1)
+              (iter (- trials-remaining 1) (+ trials-passed 1) x2))
+            (else
+              (iter (- trials-remaining 1) trials-passed x2))))))
+  (iter trials 0 initial-x))
+
+(estimate-pi 1000000)
