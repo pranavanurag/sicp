368be7fea921036e5a49678fe5d72077665296ab
Pranav Anurag <anuragpranav@gmail.com>
2023-05-30 12:57:06 +0530

    wip 3.6

--- script.scm
+++ script.scm
@@ -1,31 +1,28 @@
-(define (random-in-range low high)
-  (let ((range (- high low)))
-    (lambda () (+ low (random range)))))
+(define modulus (make-parameter (expt 2 64)))
+(define multiplier (make-parameter 6364136223846793005))
+(define increment (make-parameter 1442695040888963407))
 
-(define (unit-circle-predicate x-generator y-generator)
-  (let ((x (x-generator)) (y (y-generator)))
-    (<
-      (+ (square (- x 1.0)) (square (- y 1.0)))
-      (square 1.0))))
-  
-(define (estimate-integral predicate x1 x2 y1 y2 trials)
-  (define rect-area (* (- x2 x1) (- y2 y1)))
-  (define (integral-experiment)
-    (predicate (random-in-range x1 x2) (random-in-range y1 y2)))
-  (* rect-area (monte-carlo trials integral-experiment)))
+; this rand-update actually sucks, but for this exercise it doesn't really matter 
+(define (rand-update x)
+  (modulo (+ (* (multiplier) x) (increment)) (modulus)))
+
+(define (rand mode)
+  (define x 42)
+
+  (define generator
+    (begin (set! x (rand-update x))
+    x))
+
+  (define (init new-init)
+    (set! x new-init))
+
+  (cond
+    ((eq? mode 'generate) generator)
+    ((eq? mode 'reset) init)
+    (else (error "invalid mode" mode))))
 
 
-(define (estimate-pi trials)
-  (estimate-integral
-    unit-circle-predicate
-    0.0 2.0 0.0 2.0
-    trials))
-
-(define (monte-carlo trials experiment)
-  (define (iter trials-remaining trials-passed)
-    (cond ((= trials-remaining 0) (/ trials-passed trials))
-          ((experiment) (iter (- trials-remaining 1) (+ trials-passed 1)))
-          (else (iter (- trials-remaining 1) trials-passed))))
-    (iter trials 0))
-
-(estimate-pi 100000)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
+(rand 'generate)
