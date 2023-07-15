e02f96ec0e66dd919cdbbd490244a1ef3319cec3
Pranav Anurag <anuragpranav@gmail.com>
2023-07-15 23:59:16 +0530

    script.scm - wip 2.73

--- script.scm
+++ script.scm
@@ -1,36 +1,27 @@
-(define (make-account balance password)
-  (define (withdraw amount)
-    (if (>= balance amount)
-      (begin (set! balance (- balance amount)) balance)
-      "Insufficient funds"))
+(define (deriv exp var)
+  (cond
+    ((number? exp ) 0)
+    ((variable? exp) (if (same-variable? exp var) 1 0))
+    (else ((get 'deriv (operator exp)) (operands exp) var))))
 
-  (define (deposit amount)
-    (set! balance (+ balance amount))
-    balance)
 
-  (define (deny amount) "Incorrect password")
-    
-  (define (dispatch p m)
-    (if (eq? password p)
-      (cond
-        ((eq? m 'withdraw) withdraw)
-        ((eq? m 'deposit) deposit)
-        (else (error "Unknown request: MAKE-ACCOUNT" m)))
-      deny))
+;a. conditionals on the kinds of operators that the deriv procedure may handle
+;   are now kept separate from this procedures body
+;b.
+(define (install-deriv-procedures)
+  ((put 'deriv '+ deriv-sum)
+   (put 'deriv '* deriv-product)))
 
-  dispatch)
+(define (deriv-sum exp var)
+  (make-sum (deriv (addend exp) var)
+            (deriv (augend exp) var)))
 
-(define (make-joint master-acc master-password joint-password)
-  (define (proxy p m)
-    (if (eq? joint-password p)
-      (master-acc master-password m)
-      (error "incorrect password!")))
-  proxy)
+(define (deriv-product exp var)
+  (make-sum (make-product
+             (multiplier exp)
+             (deriv (multiplicand exp) var))
+            (make-product
+             (deriv (multiplier exp) var)
+             (multiplicand exp))))
 
-(define peter-acc (make-account 100 'kingcrimson))
-((peter-acc 'kingcrimson 'withdraw) 10)
-((peter-acc 'kingcrimson 'withdraw) 10)
-((peter-acc 'kingcrimson 'withdraw) 10)
-
-(define proxy-acc (make-joint peter-acc 'kingcrimson 'afx))
-((proxy-acc 'afx 'withdraw) 10)
+(install-deriv-procedures)
