42c8c79d914fe01bfb239782c10ddbb1fa0d74ee
Pranav Anurag <anuragpranav@gmail.com>
2024-05-18 22:33:37 +0530

    script.scm - working data dispatch example

--- script.scm
+++ script.scm
@@ -1,39 +1,84 @@
-;; the main hindarance in solving 2.73 is the absence of a get and put (to table) as described in the book
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record (cdr record) #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table))))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
 
-(define table '())
+(define operation-table (make-table))
 
-(define already-exists?
-  (lambda (key)
-    (if (assoc key table) 
-        #t 
-        #f)))
+(define (get key-1 key-2)
+  ((operation-table 'lookup-proc) key-1 key-2))
 
-(define update-existing
-  (lambda (key value)
-    (set! table (cons (cons key value) (remove-pair table key)))))
+(define (put key-1 key-2 value)
+  ((operation-table 'insert-proc!) key-1 key-2 value))
 
-(define remove-pair
-  (lambda (table key)
-    (filter (lambda (item) 
-              (not (eq? key (car item)))) 
-            table)))
+  (define (attach-tag type-tag contents)
+  (cons type-tag contents))
 
-(define create-entry
-  (lambda (key value)
-    (set! table (cons (cons key value) table))))
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
 
-(define put
-  (lambda (key value)
-    (if (already-exists? key)
-        (update-existing key value)
-        (create-entry key value))))
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum)))
 
-(define get 
-  (lambda (key) 
-    (cdr (assoc key table))))
+(define (install-polar-package)
+;; internal procedures
+(define (magnitude z) (car z))
+(define (angle z) (cdr z))
+(define (make-from-mag-ang r a) (cons r a))
+(define (real-part z) (* (magnitude z) (cos (angle z)))) (define (imag-part z) (* (magnitude z) (sin (angle z)))) (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+;; interface to the rest of the system
+(define (tag x) (attach-tag 'polar x))
+(put 'real-part '(polar) real-part) 
+(put 'imag-part '(polar) imag-part)
+ (put 'magnitude '(polar) magnitude) 
+(put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+(lambda (x y) (tag (make-from-real-imag x y))))
+(put 'make-from-mag-ang 'polar (lambda (r a) (tag (make-from-mag-ang r a)))) 'done)
 
-; this works but the book wants
-;(put ⟨op ⟩ ⟨type ⟩ ⟨item ⟩) installs the ⟨item ⟩ in the table, indexed by the ⟨op ⟩ and the ⟨type ⟩.
-; (get ⟨op ⟩ ⟨type ⟩) looks up the ⟨op ⟩, ⟨type ⟩ entry in the table and returns the item found there. If no item is found, get returns false.
 
-; my key is of a different 'type' WIP
+(define (apply-generic op . args)
+(let ((type-tags (map type-tag args)))
+(let ((proc (get op type-tags))) (if proc
+          (apply proc (map contents args))
+          (error "No method for these types: APPLY-GENERIC"
+            (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z)) 
+(define (imag-part z) (apply-generic 'imag-part z)) 
+(define (magnitude z) (apply-generic 'magnitude z)) 
+(define (angle z) (apply-generic 'angle z))
+
+
+
+(install-polar-package)
+
+(define my-crazy-complex-number ((get 'make-from-mag-ang 'polar) 12 12))
+(real-part my-crazy-complex-number)
