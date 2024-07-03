1ac94f9b5a40e2e7c337931278e2f54d3cc83a50
Pranav Anurag <anuragpranav@gmail.com>
2024-07-04 02:28:41 +0530

    script.scm - reading 2.4.3

--- script.scm
+++ script.scm
@@ -32,7 +32,7 @@
 (define (put key-1 key-2 value)
   ((operation-table 'insert-proc!) key-1 key-2 value))
 
-  (define (attach-tag type-tag contents)
+(define (attach-tag type-tag contents)
   (cons type-tag contents))
 
 (define (type-tag datum)
@@ -44,41 +44,3 @@
   (if (pair? datum)
       (cdr datum)
       (error "Bad tagged datum -- CONTENTS" datum)))
-
-(define (install-polar-package)
-;; internal procedures
-(define (magnitude z) (car z))
-(define (angle z) (cdr z))
-(define (make-from-mag-ang r a) (cons r a))
-(define (real-part z) (* (magnitude z) (cos (angle z)))) (define (imag-part z) (* (magnitude z) (sin (angle z)))) (define (make-from-real-imag x y)
-    (cons (sqrt (+ (square x) (square y)))
-          (atan y x)))
-;; interface to the rest of the system
-(define (tag x) (attach-tag 'polar x))
-(put 'real-part '(polar) real-part) 
-(put 'imag-part '(polar) imag-part)
- (put 'magnitude '(polar) magnitude) 
-(put 'angle '(polar) angle)
-  (put 'make-from-real-imag 'polar
-(lambda (x y) (tag (make-from-real-imag x y))))
-(put 'make-from-mag-ang 'polar (lambda (r a) (tag (make-from-mag-ang r a)))) 'done)
-
-
-(define (apply-generic op . args)
-(let ((type-tags (map type-tag args)))
-(let ((proc (get op type-tags))) (if proc
-          (apply proc (map contents args))
-          (error "No method for these types: APPLY-GENERIC"
-            (list op type-tags))))))
-
-(define (real-part z) (apply-generic 'real-part z)) 
-(define (imag-part z) (apply-generic 'imag-part z)) 
-(define (magnitude z) (apply-generic 'magnitude z)) 
-(define (angle z) (apply-generic 'angle z))
-
-
-
-(install-polar-package)
-
-(define my-crazy-complex-number ((get 'make-from-mag-ang 'polar) 12 12))
-(real-part my-crazy-complex-number)
