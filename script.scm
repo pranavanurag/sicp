f5cc0e4f78d12b5856d4dcecc0d1699181c7c98e
Pranav Anurag <anuragpranav@gmail.com>
2024-07-23 01:04:29 +0530

    script.scm - fuKc you

--- script.scm
+++ script.scm
@@ -73,52 +73,54 @@
       (cdr datum)
       (error "Bad tagged datum -- CONTENTS" datum)))
 
-;; real and imaginary parts representation
-(define (real-part-rectangular z) (car z))
-(define (imag-part-rectangular z) (cdr z))
-(define (magnitude-rectangular z) (sqrt (+ (square (real-part-rectangular z)) (square (imag-part-rectangular z)))))
-(define (angle-rectangular z) (atan (imag-part-rectangular z) (real-part-rectangular z)))
-(define (make-from-real-imag-rectangular x y) (attach-tag 'rectangular (cons x y)))
-(define (make-from-mag-ang-rectangular r a) (make-from-real-img-rectangular (* r (cos a)) (* r (sin a))))
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+  (sqrt (+
+    (square (real-part z))
+    (square (imag-part z)))))
+  (define (angle z) (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a) (cons (* r (cos a)) (* r (sin a))))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+    (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(install-rectangular-package)
 
 
-;; magnitude and angle representation
-(define (real-part-polar z) (* (cos (angle-polar z)) (magnitude-polar z)))
-(define (imag-part-polar z) (* (sin (angle-polar z)) (magnitude-polar z)))
-(define (magnitude-polar z) (car z))
-(define (angle-polar z) (cdr z))
-(define (make-from-real-imag-polar x y)
-  (make-from-mag-ang-polar (sqrt (+ (square x) (square y))) (atan y x)))
-(define (make-from-mag-ang-polar m a) (attach-tag 'polar (cons m a)))
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z) (* (magnitude z) (cos (angle z))))
+  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons
+      (sqrt (+ (square x) (square y)))
+      (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+    (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
 
-
-(define (rectangular? z) (eq? (type-tag z) 'rectangular))
-(define (polar? z) (eq? (type-tag z) 'polar))
-
-(define (real-part z)
-  (cond
-    ((rectangular? z) (real-part-rectangular (contents z)))
-    ((polar? z) (real-part-polar (contents z)))
-    (else (error "Unknown type: REAL-PART" z))))
-
-(define (imag-part z)
-  (cond
-    ((rectangular? z) (imag-part-rectangular (contents z)))
-    ((polar? z) (imag-part-polar (contents z)))
-    (else (error "Unknown type: IMAG-PART" z))))
-
-(define (magnitude z)
-  (cond
-    ((rectangular? z) (magnitude-rectangular (contents z)))
-    ((polar? z) (magnitude-polar (contents z)))
-    (else (error "Unknown type: MAGNITUDE" z))))
-
-(define (angle z)
-  (cond
-    ((rectangular? z) (angle-rectangular (contents z)))
-    ((polar? z) (angle-polar (contents z)))
-    (else (error "Unknown type: ANGLE" z))))
-
-(define (make-from-real-imag x y) (make-from-real-imag-rectangular x y))
-(define (make-from-mag-ang r a) (make-from-mag-ang-polar r a))
-
+(install-polar-package)
