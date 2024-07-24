9077b04f33a3cbbd9ca7177578e57a5ad6de353f
Pranav Anurag <anuragpranav@gmail.com>
2024-07-24 22:04:22 +0530

    fuck you all

--- script.scm
+++ script.scm
@@ -36,111 +36,21 @@
 ;; functions above this comment are assumed and not a part of current exercise
 
 
-(define (add-complex z1 z2)
-  (make-from-real-imag
-    (+ (real-part z1) (real-part z2))
-    (+ (imag-part z1) (imag-part z2)))) 
+(define (deriv exp var)
+  (cond
+    ((number? exp) 0)
+    ((variable? exp) 1)
+    (else ((get 'deriv (operator exp))
+      (operands exp) var))))
+
+(define (operator exp) (car exp))
+;; I will get over this hump
+;; fuck you all. you all fucking suck
+(define (operands exp) (cdr exp))
+;; might be helpful to copy over the boring impl for deriv from section2.3.2
+
+(define (install-deriv-package)
+  (define (internal-procedure-1 arg1 arg2) ('haha))
   
-(define (sub-complex z1 z2)
-  (make-from-real-imag
-    (- (real-part z1) (real-part z2))
-    (- (imag-part z1) (imag-part z2))))
-
-(define (mul-complex z1 z2)
-  (make-from-mag-ang
-    (* (magnitude z1) (magnitude z2))
-    (+ (angle z1) (angle z2))))
   
-(define (div-complex z1 z2)
-  (make-from-mag-ang
-    (/ (magnitude z1) (magnitude z2))
-    (- (angle z1) (angle z2))))
-
-;; the above functions work with any representation as long as they expose
-;; real-part, imag-part, magnitude, angle
-
-;; we may use type tags to sign the data objects
-(define (attach-tag type-tag contents)
-  (cons type-tag contents))
-
-(define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum -- TYPE-TAG" datum)))
-
-(define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum -- CONTENTS" datum)))
-
-(define (install-rectangular-package)
-  ;; internal procedures
-  (define (real-part z) (car z))
-  (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) (cons x y))
-  (define (magnitude z)
-  (sqrt (+
-    (square (real-part z))
-    (square (imag-part z)))))
-  (define (angle z) (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a) (cons (* r (cos a)) (* r (sin a))))
-
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'rectangular x))
-  (put 'real-part '(rectangular) real-part)
-  (put 'imag-part '(rectangular) imag-part)
-  (put 'magnitude '(rectangular) magnitude)
-  (put 'angle '(rectangular) angle)
-  (put 'make-from-real-imag 'rectangular
-    (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'rectangular
-    (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-(install-rectangular-package)
-
-
-(define (install-polar-package)
-  ;; internal procedures
-  (define (magnitude z) (car z))
-  (define (angle z) (cdr z))
-  (define (make-from-mag-ang r a) (cons r a))
-  (define (real-part z) (* (magnitude z) (cos (angle z))))
-  (define (imag-part z) (* (magnitude z) (sin (angle z))))
-  (define (make-from-real-imag x y)
-    (cons
-      (sqrt (+ (square x) (square y)))
-      (atan y x)))
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'polar x))
-  (put 'real-part '(polar) real-part)
-  (put 'imag-part '(polar) imag-part)
-  (put 'magnitude '(polar) magnitude)
-  (put 'angle '(polar) angle)
-  (put 'make-from-real-imag 'polar
-    (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'polar
-    (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-(install-polar-package)
-
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-      (let ((proc (get op type-tags)))
-        (if proc (apply proc (map contents args)) (error "kya bol raha hai bhai")))))
-
-        (define (real-part z) (apply-generic 'real-part z)) (define (imag-part z) (apply-generic 'imag-part z)) (define (magnitude z) (apply-generic 'magnitude z)) (define (angle z) (apply-generic 'angle z))
-
-
-(define (make-from-real-imag x y)
-((get 'make-from-real-imag 'rectangular) x y))
-(define (make-from-mag-ang r a)
-((get 'make-from-mag-ang 'polar) r a))
-
-(define rect-example (make-from-real-imag 12 13))
-(define polar-example (make-from-mag-ang 12 13))
-
-(define sum-up (add-complex rect-example polar-example))
-
-sum-up
+  (put 'deriv '(+) (crazy-function-to-handle-sums-which-takes-exp-and-var)))
