551daade56ca5bff295d6cf3424c1f599b814fd0
Pranav Anurag <anuragpranav@gmail.com>
2024-07-27 03:40:09 +0530

    script.scm 2.73b

--- script.scm
+++ script.scm
@@ -36,21 +36,56 @@
 ;; functions above this comment are assumed and not a part of current exercise
 
 
+(define (make-sum a1 a2)
+  (cond
+    ((=number? a1 0) a2)
+    ((=number? a2 0) a1)
+    ((and (number? a1) (number? a2)) (+ a1 a2))
+    (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond
+    ((or (=number? m1 0) (=number? m2 0)) 0)
+    ((=number? m1 1) m2)
+    ((=number? m2 1) m1)
+    ((and (number? m1) (number? m2)) (* m1 m2)) (else (list '* m1 m2))))
+
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (car s));assume s is operands pair
+(define (augend s) (cadr s));assume s is operands pair
+
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (car p)) ;assume p is operands pair
+(define (multiplicand p) (cadr p)) ;assume p is operands pair
+
+(define (install-deriv-package)
+
+  (define (sum-deriv operands var) 
+    (make-sum (deriv (augend operands) var) (deriv (addend operands) var)))
+
+  (define (product-deriv operands var) 
+    (make-sum
+      (make-product (multiplier operands) (deriv (multiplicand operands) var))
+      (make-product (multiplicand operands) (deriv (multiplier operands) var))))
+
+  (put 'deriv '+ sum-deriv)
+  (put 'deriv '* product-deriv))
+
+(install-deriv-package)
+
 (define (deriv exp var)
+  (display "\nderiv ") (display exp)
   (cond
     ((number? exp) 0)
-    ((variable? exp) 1)
+    ((variable? exp) (if (same-variable? exp var) 1 0))
     (else ((get 'deriv (operator exp))
       (operands exp) var))))
 
 (define (operator exp) (car exp))
-;; I will get over this hump
-;; fuck you all. you all fucking suck
 (define (operands exp) (cdr exp))
-;; might be helpful to copy over the boring impl for deriv from section2.3.2
 
-(define (install-deriv-package)
-  (define (internal-procedure-1 arg1 arg2) ('haha))
-  
-  
-  (put 'deriv '(+) (crazy-function-to-handle-sums-which-takes-exp-and-var)))
+(deriv '(* (+ x y) (+ x 3)) 'x)
