0957ffbce13accae3e51f4709c4ca2ca6d25bf73
Pranav Anurag <anuragpranav@gmail.com>
2024-07-31 01:16:20 +0530

    script.scm - wip 2.74

--- script.scm
+++ script.scm
@@ -34,3 +34,25 @@
 
 
 ;; functions above this comment are assumed and not a part of current exercise
+
+;; as an illustration, I am going to assume that Division 1 of Insatiable Enterprises
+;; stores (cons address salary)
+(define (make-record-div-1 add sal) (cons add sal))
+(define (address-div-1 record-div-1) (car (record-div-1)))
+(define (salary-div-1 record-div-1) (cdr (record-div-1)))
+
+;; for contrast, let's assume that Division 2 of Insatiable Enterprises
+;; stores (cons salary address)
+(define (make-record-div-2 sal add) (cons sal add))
+(define (salary-div-2 record-div-2) (car (record-div-2)))
+(define (address-div-2 record-div-2) (cdr (record-div-2)))
+
+
+;; a) just give me the set bro. and just tag yo data. that's it
+;; ok you will also need to give me your procedures for salary and address
+(define (install-div-1)
+  (define (address-div-1 record-div-1) (car (record-div-1)))
+  (define (salary-div-1 record-div-1) (cdr (record-div-1)))
+  (put 'div-1 'salary salary-div-1)
+  (put 'div-1 'address address-div-1)
+  (put 'div-1 'file ))
