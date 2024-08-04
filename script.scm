e7d851f40872a8dbe3afc733570d0f2821da71e3
Pranav Anurag <anuragpranav@gmail.com>
2024-08-05 01:15:17 +0530

    script.scm - solve 2.75

--- script.scm
+++ script.scm
@@ -1,80 +1,32 @@
-(define (make-table)
-  (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record (cdr record) #f))
-            #f)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1 (cons key-2 value))
-                            (cdr local-table))))))
-    (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            (else (error "Unknown operation -- TABLE" m))))
-    dispatch))
+(define (make-from-real-imag x y)
+    (define (dispatch op)
+        (cond
+            ((eq? op 'real-part) x)
+            ((eq? op 'imag-part) y)
+            ((eq? op 'magnitude) (sqrt (+ (square x) (square y))))
+            ((eq? op 'angle) (atan y x))
+            (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+    dispatch)
 
-(define operation-table (make-table))
-
-(define (get key-1 key-2)
-  ((operation-table 'lookup-proc) key-1 key-2))
-
-(define (put key-1 key-2 value)
-  ((operation-table 'insert-proc!) key-1 key-2 value))
+(define (apply-generic op arg) (arg op))
 
 
-;; functions above this comment are assumed and not a part of current exercise
+(define mynum (make-from-real-imag 12 13))
+(apply-generic 'magnitude mynum)
 
 
-;; suppose that each division's personnel records consist of a single file
-;; which contains a set of records keyed on employees’ names
-;; the structure of the set varies from division to division
-;; (this means that the implementation of `set` used should be different in our example)
-;; I am not going to write out two set implementations and use them to create example datasets
-;; borrowing from code_report on youtube, I am going to use qouted lists to create data
+(define (make-from-mag-ang m a)
+    (define (dispatch op)
+        (cond
+            ((eq? op 'real-part) (* m (cos a)))
+            ((eq? op 'imag-part) (* m (sin a)))
+            ((eq? op 'magnitude) m)
+            ((eq? op 'angle) a)
+            (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
+    dispatch)
 
-
-;; division 1
-;; a record here is a list stored against a key which is the ID of that record
-(define set-of-records-div-1
-  (list
-    '("Pranav Anurag" ("M" 120 "address1"))
-    '("Surabhi Anurag" ("F" 150 "address2"))
-    '("Anurag Subhash" ("M" 170 "address3"))))
-
-(define (key-div-1 record) (car record))
-(define (get-record-div-1 given-key set-of-records)
-  (newline) (display (key-div-1 (car set-of-records)))
-  (cond
-    ((null? set-of-records) #f)
-    ((equal? (key-div-1 (car set-of-records)) given-key) (car set-of-records))
-    (else (get-record-div-1 given-key (cdr set-of-records)))))
-(get-record-div-1 "id1" set-of-records-div-1)
-
-
-;; division 2
-;; a record here is a list with the id as the first element of the record list
-(define set-of-records-div-2
-  (list
-    '(("Anvita" "Anurag") "addr20" "name20" 140)
-    '( "addr21" "name21" 143)
-    '("id22" "addr22" "name22" 160)))
-
-(define (key-div-2 record) (car record))
-(define (get-record-div-2 given-key set-of-records)
-  (newline) (display (key-div-1 (car set-of-records)))
-  (cond
-    ((null? set-of-records) #f)
-    ((equal? (key-div-1 (car set-of-records)) given-key) (car set-of-records))
-    (else (get-record-div-1 given-key (cdr set-of-records)))))
-(get-record-div-2 "id20" set-of-records-div-2)
+(define mynum2 (make-from-mag-ang 12 13))
+(apply-generic 'magnitude mynum2)
+(apply-generic 'angle mynum2)
+(apply-generic 'real-part mynum2)
+(apply-generic 'imag-part mynum2)
