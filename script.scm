ea9a9624d81bbfe9181eb9a2c72c8120c89e9627
Pranav Anurag <anuragpranav@gmail.com>
2024-08-23 22:58:06 +0530

    script.scm for 3.8

--- script.scm
+++ script.scm
@@ -1,32 +1,32 @@
-(define (make-from-real-imag x y)
-    (define (dispatch op)
-        (cond
-            ((eq? op 'real-part) x)
-            ((eq? op 'imag-part) y)
-            ((eq? op 'magnitude) (sqrt (+ (square x) (square y))))
-            ((eq? op 'angle) (atan y x))
-            (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
-    dispatch)
-
-(define (apply-generic op arg) (arg op))
+;;;Exercise 3.8: When we defined the evaluation model in Section 1.1.3
+;; we said that the first step in evaluating an expression is to evaluate its subexpressions
+;; But we never specified the order in which the subexpressions should be evaluated
+;; (e.g., le to right or right to le)
+;; When we in- troduce assignment, the order in which the arguments to a procedure are evaluated can make a difference
+;; to the result. Define a simple procedure f such that evaluating
+;; (+ (f 0) (f 1))
+;; will return 0 if the arguments to + are evaluated from le to right
+    ;; but will return 1 if the arguments are evaluated from right to le.
 
 
-(define mynum (make-from-real-imag 12 13))
-(apply-generic 'magnitude mynum)
+;;define a scope with let inside the function
+;; everytime the function is invoked this scope becomes available for the lambda that is returned by f
+(define f
+  (let ((seed 1))
+    (lambda (x)
+      (cond
+        ((eq? x 0) (set! seed 0) seed)
+        (else seed)))))
 
+(f 0)
+(f 1)
 
-(define (make-from-mag-ang m a)
-    (define (dispatch op)
-        (cond
-            ((eq? op 'real-part) (* m (cos a)))
-            ((eq? op 'imag-part) (* m (sin a)))
-            ((eq? op 'magnitude) m)
-            ((eq? op 'angle) a)
-            (else (error "Unknown op: MAKE-FROM-MAG-ANG" op))))
-    dispatch)
+(define f
+  (let ((seed 1))
+    (lambda (x)
+      (cond
+        ((eq? x 0) (set! seed 0) seed)
+        (else seed)))))
 
-(define mynum2 (make-from-mag-ang 12 13))
-(apply-generic 'magnitude mynum2)
-(apply-generic 'angle mynum2)
-(apply-generic 'real-part mynum2)
-(apply-generic 'imag-part mynum2)
+(f 1)
+(f 0)
